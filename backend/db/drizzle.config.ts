a9428145608433be3be838efbf80ec41dda84b46
Wilksy <a.wilksy92@gmail.com>
2025-10-23 23:52:38 +1000

    fix: update database connection configuration to use config.DB_FILE_NAME

--- backend/db/drizzle.config.ts
+++ backend/db/drizzle.config.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/libsql";
+import { config } from "@config/backend";
 
 const db = drizzle({
-  connection: { url: Deno.env.get("DB_FILE_NAME")! },
+  connection: { url: config.DB_FILE_NAME },
 });
 
 export { db };
