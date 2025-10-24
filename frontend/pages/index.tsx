7d3882fd33f88c3bd2bcdb1037a4e9b47c2a4e8a
Wilksy <a.wilksy92@gmail.com>
2025-10-24 21:15:02 +1000

    feat: update Index component layout with HeroText and welcome message

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import HeroText from "@ui/HeroText.tsx";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -6,8 +7,26 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
+    <section className="flex flex-col items-center justify-center py-32 px-4 text-(--text) max-w-2xl mx-auto text-center">
+      <h2 className="text-4xl font-semibold text-center">Welcome to</h2>
+      <HeroText
+        text="OpenAgent"
+        className="text-8xl font-semibold text-center dark:text-green-400 text-green-500 pb-10 mx-auto"
+        delay={100}
+        duration={0.6}
+        ease="power3.out"
+        splitType="chars"
+        from={{ opacity: 0, y: 40 }}
+        to={{ opacity: 1, y: 0 }}
+        threshold={0.1}
+        rootMargin="-100px"
+        textAlign="center"
+        tag="h1"
+      />
+      <p className="text-lg text-(--muted)">
+        We've been around since 2013, and our vision is to make it easy for
+        people to <strong>Buy, Sell and Own Property.</strong>
+      </p>
+    </section>
   );
 }
