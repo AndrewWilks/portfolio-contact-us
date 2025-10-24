68cd7e1c9cfd6ec60769b6392e4b59039baede79
Wilksy <a.wilksy92@gmail.com>
2025-10-24 12:11:15 +1000

    fix(config): update default DB_FILE_NAME format; improve CI test command

--- .config/backend.config.ts
+++ .config/backend.config.ts
@@ -16,7 +16,7 @@ function loadConfig(): Config {
   }
 
   const config = {
-    DB_FILE_NAME: Deno.env.get("DB_FILE_NAME") || "./database.sqlite",
+    DB_FILE_NAME: Deno.env.get("DB_FILE_NAME") || "file:./database.sqlite",
     NODE_ENV: Deno.env.get("NODE_ENV") || "development",
     BACKEND_PORT: Deno.env.get("BACKEND_PORT") || 8000,
   };
